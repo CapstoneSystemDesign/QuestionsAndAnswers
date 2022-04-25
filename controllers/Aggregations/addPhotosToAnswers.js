debbe672ca0765a0fcf2fc1eb4a7d99cc01e156f
Vince <vincent.tran.ece@gmail.com>
2022-04-25 09:27:25 -0500

    saves progress

---
+++ controllers/Aggregations/addPhotosToAnswers.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    '$lookup': {
+      'from': 'answers_photos',
+      'localField': 'answer_id',
+      'foreignField': 'answer_id',
+      'as': 'photos'
+    }
+  }
+];
+
+MongoClient.connect(
+  'mongodb://localhost:27017/',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function(connectErr, client) {
+    assert.equal(null, connectErr);
+    const coll = client.db('System-Design-Capstone').collection('answers');
+    coll.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+    });
+    client.close();
+  });
+
+// const mongoose = require('mongoose');
+// const { AnswersList } = require('../../models/model-index');
+
+// aggregate.lookup({ from: 'AnswersList.answers_photos', localField: 'answer_id', foreignField: 'answer_id', as: 'photos' });
